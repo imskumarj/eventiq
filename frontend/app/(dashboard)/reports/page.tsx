a10a73e9e0e134e54c4e0f04539924871200e76d
Sudhansu Kumar <imskumarj@gmail.com>
2026-03-17 22:54:34 +0530

    Frontend page updated for reports

--- frontend/app/(dashboard)/reports/page.tsx
+++ frontend/app/(dashboard)/reports/page.tsx
@@ -3,8 +3,8 @@
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 
-import { Button } from "../../../components/ui/button";
-import { Badge } from "../../../components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 
 import {
   FileText,
@@ -18,7 +18,7 @@ import {
   generateReport,
   getReports,
   downloadReport,
-} from "../../../services/reports";
+} from "@/services/reports";
 
 /* ---------------- TYPES ---------------- */
 
